0d8452e7e4ce92a4cec889ee938e8456227ad297
Michael Qiu <michaelqiu@MichaelMacBook-Pro-HOME.local>
2025-06-11 15:25:02 +0800

    rename pages

--- miniprogram/pages/home/home.ts
+++ miniprogram/pages/home/home.ts
@@ -1,36 +1,95 @@
-import { fetchLogin } from "../../utils/api";
-import { setToken, setOpenID, setUserID } from "../../utils/auth";
-import { goRegister } from "../../utils/navigateHelper";
+import { AreaChangeDetail } from "../register/register";
 
 Page({
-  async onLogin() {
-    wx.login({
-      success: async (res) => {
-        console.log(res);
-        if (res.code) {
-          const requestConfig = { code: res.code };
-
-          try {
-            const response = await fetchLogin(requestConfig);
-            if (response.code === 0 && response.data.accessToken) {
-              setToken(response.data.accessToken);
-            }
-            if (response.code === 0 && response.data.openid) {
-              setOpenID(response.data.openid);
-            }
-            if (response.code === 0 && response.data.userId) {
-              setUserID(response.data.userId);
-            }
-
-            return goRegister();
-          } catch (err) {
-            console.error("登录请求失败：", err);
-          }
-        }
+  data: {
+    position: "通州",
+    home: {
+      options: {
+        indicatorDots: false,
+        vertical: true,
+        autoplay: false,
+        interval: 2000,
+        duration: 500,
       },
-      fail(err) {
-        console.error("wx.login 调用失败", err);
-      },
+      items: [
+        {
+          path: "http://swik7nyin.hd-bkt.clouddn.com/swiper-item.jpg",
+          info: {
+            id: 0,
+            name: "甜七七1",
+          },
+          activity: {
+            name: "南山飞盘暴汗局1",
+            status: 1,
+          },
+        },
+        {
+          path: "http://swik7nyin.hd-bkt.clouddn.com/swiper-item.jpg",
+          info: {
+            id: 1,
+            name: "甜七七2",
+          },
+          activity: {
+            name: "南山飞盘暴汗局2",
+            status: 0,
+          },
+        },
+        {
+          path: "http://swik7nyin.hd-bkt.clouddn.com/swiper-item.jpg",
+          info: {
+            id: 2,
+            name: "甜七七3",
+          },
+        },
+      ],
+    },
+  },
+  showRegionPicker(
+    e: WechatMiniprogram.CustomEvent<{
+      currentTarget: { dataset: { field: string } };
+    }>
+  ) {
+    const field = e.currentTarget.dataset.field;
+
+    this.selectComponent("#areaPicker").onAreaPicker(field);
+  },
+
+  onPositionChange(e: WechatMiniprogram.CustomEvent<AreaChangeDetail>) {
+    const { text, value } = e.detail;
+
+    const positionOptions: Option = {
+      label: text.join("-"),
+      value: value.join("-"),
+    };
+
+    console.log(positionOptions);
+
+    this.setData({
+      position: text[2],
     });
+  },
+
+  onTabChange(e: any) {
+    const idx = e.detail.index;
+    console.log(idx);
+    switch (idx) {
+      case 0:
+        console.log(idx);
+        // 已经在首页，可以不做任何处理，或者直接 reLaunch 保证回到根
+        // wx.reLaunch({ url: "/pages/index/index" });
+        break;
+      case 1:
+        console.log(idx);
+        // wx.reLaunch({ url: "/pages/category/category" });
+        break;
+      case 2:
+        console.log(idx);
+        // wx.reLaunch({ url: "/pages/cart/cart" });
+        break;
+      case 3:
+        console.log(idx);
+        // wx.reLaunch({ url: "/pages/profile/profile" });
+        break;
+    }
   },
 });
